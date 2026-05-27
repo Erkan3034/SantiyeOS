d83be231a8aaf4127947f6b3b5e61aad3b78b086
Erkan3034 <turguterkan55@gmail.com>
2026-05-27 23:52:03 +0300

    Update `CorsConfig` to differentiate between production and development environments

--- src/main/java/com/santiyeos/api/config/CorsConfig.java
+++ src/main/java/com/santiyeos/api/config/CorsConfig.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origins}")
+    @Value("${app.cors.allowed-origins}") //prod - dev ayrimi yapildi.
     private String allowedOrigins;
 
     @Bean
